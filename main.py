114c6625351d4a292dc76d32e522da14920bd82f
Prakhar Swarnakar <lovyswarnakar@gmail.com>
2023-07-15 21:42:50 +0530

    basic http with raw json body

--- main.py
+++ main.py
@@ -1,7 +1,17 @@
 from fastapi import FastAPI
+from fastapi.params import Body
 
 app = FastAPI()
 
-@app. get('/')
+@app.get('/')
 async def root():
-    return {"message":"Hello Worlds"}
+    return {"message": "Hello World"}
+
+@app.get('/issue')
+async def issue_details():
+    return {"issue_details": "These are the details of the book issue"}
+
+@app.post("/issuebook")
+async def issue_book(payload: dict= Body(...)):
+    print(payload)
+    return {"new_book_issued": f"successfully Issued {payload['title']}"}
